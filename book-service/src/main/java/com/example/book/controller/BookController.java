fd1104bbdc84962e2ce9019973245dde62999b6e
Sherzod Mamadaliev <egnaf@yahoo.com>
2021-01-06 17:06:12 +0300

    Fix roles name to uppercase #7

--- book-service/src/main/java/com/example/book/controller/BookController.java
+++ book-service/src/main/java/com/example/book/controller/BookController.java
@@ -3,9 +3,7 @@ package com.example.book.controller;
 import com.example.book.service.BookService;
 import com.example.book.util.CurrentUserUtil;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,15 +21,15 @@ public class BookController {
     }
 
     @GetMapping("/user")
-    @PreAuthorize("hasRole('user')")
+    @PreAuthorize("hasRole('USER')")
     public String user() {
-        return "Books of " + CurrentUserUtil.getNickName() + " _ " + CurrentUserUtil.getId();
+        return "Books of " + CurrentUserUtil.getNickName();
     }
 
     @GetMapping("/admin")
-    @PreAuthorize("hasRole('admin')")
-    public ResponseEntity<?> admin() {
-        return ResponseEntity.ok(SecurityContextHolder.getContext().getAuthentication());
+    @PreAuthorize("hasRole('ADMIN')")
+    public String admin() {
+        return "Books of " + CurrentUserUtil.getNickName();
     }
 
 }
